114ef6cc746aaf88b415ae740c1a2c6062b42bb0
VijayYadav1999 <vijay5602224@gmail.com>
2023-05-15 18:14:12 +0530

    Update dataupload.py

--- Analyser/dataupload.py
+++ Analyser/dataupload.py
@@ -2,9 +2,7 @@ import json
 
 # Example data as string
 data = {
-    "name": "John Doe",
-    "age": "25",
-    "occupation": "Developer"
+    "name": "Embrace the beauty of each moment, for in your happiness lies the infinite potential to inspire and uplift those around you."
 }
 
 # Save the data to a JSON file
